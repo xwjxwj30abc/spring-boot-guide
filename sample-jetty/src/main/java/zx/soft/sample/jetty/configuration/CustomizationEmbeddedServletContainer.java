d6fb765943fa9d495cce313723b3368cc0587f21
xuwenjuan <xwjfznxl@gmail.com>
2015-09-24 15:07:25 +0800

    简化sample-jetty项目jetty使用配置示例

--- sample-jetty/src/main/java/zx/soft/sample/jetty/configuration/CustomizationEmbeddedServletContainer.java
+++ sample-jetty/src/main/java/zx/soft/sample/jetty/configuration/CustomizationEmbeddedServletContainer.java
@@ -1,13 +1,8 @@
 package zx.soft.sample.jetty.configuration;
 
-import java.text.SimpleDateFormat;
-import java.util.Locale;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -23,13 +18,6 @@ public class CustomizationEmbeddedServletContainer implements EmbeddedServletCon
 	public void customize(ConfigurableEmbeddedServletContainer container) {
 		container.setPort(Integer.parseInt(port));
 		container.setSessionTimeout(Integer.parseInt(sessionTimeout));
-	}
-
-	@Bean
-	public Jackson2ObjectMapperBuilder objectMapperBuilder() {
-		Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
-		builder.dateFormat(new SimpleDateFormat("EEE MMM dd HH:mm:ss zzz yyyy", Locale.US));
-		return builder;
 	}
 
 }
--- sample-jetty/src/main/java/zx/soft/sample/jetty/web/TestController.java
+++ sample-jetty/src/main/java/zx/soft/sample/jetty/web/TestController.java
@@ -1,29 +1,16 @@
 package zx.soft.sample.jetty.web;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import zx.soft.sample.jetty.service.Writer;
-
 @Controller
 public class TestController {
-
-	@Autowired
-	private Writer writer;
 
 	@RequestMapping("/hello")
 	@ResponseBody
 	String home() {
 		return "Hello World!";
-	}
-
-	@RequestMapping(value = "/hbase")
-	@ResponseBody
-	public String put() {
-		writer.write("0", "qua", "value");
-		return "ok";
 	}
 
 }
